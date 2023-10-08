b4b961cdff22ca77cc71adb853fa9e297cc4d585
Kamil Sajdera <kamilsajdera@gmail.com>
2023-10-08 19:11:56 +0200

    Added price to CasseroleItem

--- src/components/Other/CasseroleItem.js
+++ src/components/Other/CasseroleItem.js
@@ -1,38 +1,15 @@
 import classes from './OtherItems.module.css';
 
 import casseroleImg from '../../assets/other/zapiekanki.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const CasseroleItem = () => {
     return (
         <div className={classes['other-item']}>
                 <img src={casseroleImg} alt="Casserole" />
                 <h3>Casserole</h3>
-                <div className={classes['fries-option']}>
-                    <div>Small (200g)</div>
-                    <div className={classes.price}>$2.30</div>
-                    <div className={classes.friesAdd}>
-                        <FontAwesomeIcon icon={faPlus} />
-                    </div>
-                </div>
-
-                <div className={classes['fries-option']}>
-                    <div>Mid (300g)</div>
-                    <div className={classes.price}>$4.50</div>
-                    <div className={classes.friesAdd}>
-                        <FontAwesomeIcon icon={faPlus} />
-                    </div>
-                </div>
-
-                <div className={classes['fries-option']}>
-                    <div>Small (400g)</div>
-                    <div className={classes.price}>$6.20</div>
-                    <div className={classes.friesAdd}>
-                        <FontAwesomeIcon icon={faPlus} />
-                    </div>
-                </div>
-            </div>
+                <h2 className={classes.price} style={{fontSize: "1.45rem"}}>$4.00</h2>
+                <p className={classes.description}>Thanks to fresh ingredients and their high quality, the taste of our casserole is amazing!</p>
+        </div>
     )
 };
 
