ddcfd2e4d40cf690fba2017a8900c953155b992e
Kamil Sajdera <kamilsajdera@gmail.com>
2023-10-18 16:46:36 +0200

    Logic of added the item to cart initial done.

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,14 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import isEqual from 'lodash/isEqual';
 
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
-        items: [],
+        items: [], 
         totalItems: 0
     },
     reducers: {
         addItemToCart(state, action) {
-            /// logic adding to cart soon...
+            const newItem = {itemData: action.payload, amount: 1};
+
+            const existingItemIndex = state.items.findIndex(item => isEqual(item.itemData, newItem.itemData))
+            
+            if(existingItemIndex === -1) {
+                console.log("Item no exist. Adding to array...")
+                state.items.push(newItem)
+            } else {
+                console.log("Item exist. Editing it's amount (+1)")
+                state.items[existingItemIndex].amount += 1;
+            }
+
+            state.totalItems++;
         }
     }
 });
