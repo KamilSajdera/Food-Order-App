81cf87e483c259b5a9239bc46b2290f99c4530a6
Kamil Sajdera <kamilsajdera@gmail.com>
2023-10-18 16:55:58 +0200

    Added amount of items in sidebar near option 'Basket'

--- src/components/Sidebar/SidebarMenu.js
+++ src/components/Sidebar/SidebarMenu.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
@@ -7,6 +9,18 @@ import classes from './SidebarMenu.module.css';
 import classes2 from './SidebarList.module.css';
 
 const SidebarMenu = () => {
+
+    const totalCartAmount = useSelector(state => state.cart.totalItems);
+
+    const [basketAmountClasses, setBasketAmountClasses] = useState(classes.amountBasket)
+
+    useEffect(() => {
+        setBasketAmountClasses(`${classes.amountBasket} ${classes.animationJump}`);
+
+        setTimeout(() => {
+            setBasketAmountClasses(classes.amountBasket);
+        }, 300)
+    }, [totalCartAmount])
 
     return (
         <ul  className={classes.sidebarNavi}>
@@ -19,7 +33,10 @@ const SidebarMenu = () => {
             <li>
                 <NavLink to='/basket' className={ ({isActive}) =>  isActive ? classes2.active : undefined }>
                     <FontAwesomeIcon icon={faShoppingBasket} />
-                    <span className={classes.naviTitle}>Basket</span>
+                    <span className={classes.naviTitle}>
+                    Basket
+                    <div className={basketAmountClasses}>{totalCartAmount}</div>
+                    </span>
                 </NavLink> 
             </li>
         </ul>
