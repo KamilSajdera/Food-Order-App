e393ca5722f76d168c91ce4e5a078295bd82f145
Kamil Sajdera <kamilsajdera@gmail.com>
2023-10-20 17:49:08 +0200

    Items in Casserole and Fries now add to cart.

--- src/components/Other/CasseroleItem.js
+++ src/components/Other/CasseroleItem.js
@@ -1,13 +1,21 @@
+import { useDispatch } from 'react-redux'
 import classes from './OtherItems.module.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import casseroleImg from '../../assets/other/zapiekanki.png';
 
+import { cartActions } from '../../store/cart-slice';
+
 const CasseroleItem = () => {
 
+    const dispatch = useDispatch();
+
     const addToCart = () => {
-        /// soon..
+        dispatch(cartActions.addItemToCart({
+            name: "Casserole",
+            price: 4.00
+        }))
     }
     
     return (
--- src/components/Other/FriesItem.js
+++ src/components/Other/FriesItem.js
@@ -1,10 +1,27 @@
+import { useDispatch } from 'react-redux';
 import classes from './OtherItems.module.css';
 
 import friesImg from '../../assets/other/fries.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
+import { cartActions } from '../../store/cart-slice';
+
 const FriesItem = () => {
+
+    const dispatch = useDispatch();
+
+    const addFriesToCart = (size, price) => {
+
+        const friesSummary = {
+            name: "Fries",
+            size,
+            price
+        }
+
+        dispatch(cartActions.addItemToCart(friesSummary))
+    };
+
     return (
         <div className={classes['other-item']}>
                 <img src={friesImg} alt="fries" />
@@ -12,7 +29,7 @@ const FriesItem = () => {
                 <div className={classes['fries-option']}>
                     <div>Small (200g)</div>
                     <div className={classes.price}>$2.30</div>
-                    <div className={classes.friesAdd}>
+                    <div className={classes.friesAdd} onClick={() => addFriesToCart('small (200g)', 2.30)}>
                         <FontAwesomeIcon icon={faPlus} />
                     </div>
                 </div>
@@ -20,15 +37,15 @@ const FriesItem = () => {
                 <div className={classes['fries-option']}>
                     <div>Mid (300g)</div>
                     <div className={classes.price}>$4.50</div>
-                    <div className={classes.friesAdd}>
+                    <div className={classes.friesAdd} onClick={() => addFriesToCart('mid (300g)', 4.50)}>
                         <FontAwesomeIcon icon={faPlus} />
                     </div>
                 </div>
 
                 <div className={classes['fries-option']}>
-                    <div>Small (400g)</div>
+                    <div>Big (400g)</div>
                     <div className={classes.price}>$6.20</div>
-                    <div className={classes.friesAdd}>
+                    <div className={classes.friesAdd} onClick={() => addFriesToCart('big (400g)', 6.20)}>
                         <FontAwesomeIcon icon={faPlus} />
                     </div>
                 </div>
