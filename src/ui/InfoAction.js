20768d0a9243878fb8297df52779eeb3923ac23b
Kamil Sajdera <kamilsajdera@gmail.com>
2023-11-03 13:39:18 +0100

    Added UI hook witch displaying message when item was added/removed from cart

---
+++ src/ui/InfoAction.js
@@ -0,0 +1,16 @@
+import classes from './InfoAction.module.css';
+
+const ItemAction = props => {
+
+    const barBackground = props.action === 'add' ? "#1e9138" : "#b93636"
+    const message = props.action === 'add' ? "was successfully added to cart!" : "ded"
+
+    return (
+        <div className={classes.infoBar} style={{background: barBackground}}>
+            <i><b>{props.name}</b></i> {message}
+        </div>
+    )
+};
+
+
+export default ItemAction;
