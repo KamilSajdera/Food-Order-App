4516006e0d5e0a7237e318a92116ad1aec95a4ea
Kamil Sajdera <kamilsajdera@gmail.com>
2023-11-13 16:05:09 +0100

    Improved design Fries&Caseroles within cart wrapper

--- src/components/Basket/FriesCaseroles.js
+++ src/components/Basket/FriesCaseroles.js
@@ -3,12 +3,20 @@ import CartItem from '../../ui/CartItem';
 
 const FriesCasseroles = ({items}) => {
 
+    const isSmallScreen = window.innerWidth <= 600;
+
+    const cartItemExtraStyles = {
+        width: isSmallScreen ? "43%" : "22%",
+        display: 'block', 
+        textAlign: 'center'
+    }
+
     return (
     <>
     { items.map(item => 
         <CartItem 
             price={item.price} amount={item.amount} extraPay={item.extraPay} 
-            style={{width: '20%', display: 'block', textAlign: 'center'}}> 
+            style={cartItemExtraStyles}> 
         <img src={item.img} alt={item.name} className={classes.img}/>
         <h3 style={{paddingTop: "15px"}}>{item.name}</h3>
         <p>{item.size}</p>
